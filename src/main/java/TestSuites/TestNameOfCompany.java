d8add694cb874946ad909bb2f050b2240052b90c
Eduard Anton <eduardanton78@gmail.com>
2021-02-21 01:17:28 +0700

    Create methods TestConnections

--- src/main/java/TestSuites/TestNameOfCompany.java
+++ src/main/java/TestSuites/TestNameOfCompany.java
@@ -1,4 +1,35 @@
 package TestSuites;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestNameOfCompany {
+
+    @Test
+    public void testWebConnectionLinux() {
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(2000);
+            driver.get("http://192.168.1.140:8093/SlxClient/logoff.aspx");
+    } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testWebConnectionWindows() {
+
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\selenium\\drivers\\chromedriver_88\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(2000);
+            driver.get("http://192.168.1.140:8093/SlxClient/logoff.aspx");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
