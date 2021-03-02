24678fd27e1fec54293983b6614c48b814314661
anton_en <anton-e@fbconsult.ru>
2021-03-02 23:31:26 +0700

    Fix  git pull

--- src/main/java/Tests_8092/TestOtherChangesInDoc.java
+++ src/main/java/Tests_8092/TestOtherChangesInDoc.java
@@ -255,7 +255,7 @@ public class TestOtherChangesInDoc {
             ex.printStackTrace();
         }
 
-git pull        if(Objects.equals(test_OKFS, DataComparison.compareData(okfs_type_1, okfs_type_2, okfs_type_3))) {
+        if(Objects.equals(test_OKFS, DataComparison.compareData(okfs_type_1, okfs_type_2, okfs_type_3))) {
             System.out.println("OKFS test passed!");
         }else {
             System.out.println("OKFS test failed! - X");
