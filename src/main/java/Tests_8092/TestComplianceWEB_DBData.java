f3893c7e5852bc0aa16462fa21dc077ccd0a8ef4
anton_en <anton-e@fbconsult.ru>
2021-03-02 11:58:35 +0700

    Updated TestComplianceWEB_DBData class.

--- src/main/java/Tests_8092/TestComplianceWEB_DBData.java
+++ src/main/java/Tests_8092/TestComplianceWEB_DBData.java
@@ -79,8 +79,15 @@ public class TestComplianceWEB_DBData {
     // // Variables for complianceGoverningBodies method
     String test_MANAGE_PERSON = null;
     String test_MANAGE_STRUCTURE = null;
-    String managePerson = null;
-    String manageStructure = null;
+
+    String managePerson_type_1 = null;
+    String manageStructure_type_1 = null;
+
+    String managePerson_type_2 = null;
+    String manageStructure_type_2 = null;
+
+    String managePerson_type_3 = null;
+    String manageStructure_type_3 = null;
 
 
     WebDriver driver;
@@ -537,9 +544,121 @@ public class TestComplianceWEB_DBData {
 
     }
 
-    @Test
+    @Test (groups = {"regress"},
+           dependsOnMethods = "complianceLicense")
     public void complianceGoverningBodies() {
 
+        try {
+
+            // Select the Governing Bodies tab
+            WebElement licenseTab = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[1]/div/div[8]/a"));
+            licenseTab.click();
+            Thread.sleep(2000);
+
+            // Get data of the Governing Bodies type
+            WebElement field_MANAGE_PERSON = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div[1]/div[1]/div/div/textarea"));
+            test_MANAGE_PERSON = field_MANAGE_PERSON.getAttribute("value");
+
+            WebElement field_MANAGE_STRUCTURE = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div[2]/div[1]/div/div/textarea"));
+            test_MANAGE_STRUCTURE = field_MANAGE_STRUCTURE.getAttribute("value");
+
+        } catch (InterruptedException e) {
+            System.out.println("Get web GoverningBodies data failed");
+            e.printStackTrace();
+        }
+
+        System.out.println("---------------");
+        System.out.println(test_MANAGE_PERSON);
+        System.out.println(test_MANAGE_STRUCTURE);
+        System.out.println("---------------");
+
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection(DB_Data, "SYSDBA", "masterkey");
+
+            String selectTableSQLForType_1 = "SELECT fbpomd.MANAGE_PERSON, fbpomd.MANAGE_STRUCTURE " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.MEMBERDATATYPE = '1'" +
+                    "AND fbpomd.ISPRIMARY = 'T'" +
+                    "AND fbpomd.MEMBERCLASS IS NULL";
+
+            String selectTableSQLForType_2 = "SELECT fbpomd.MANAGE_PERSON, fbpomd.MANAGE_STRUCTURE " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.MEMBERDATATYPE = '2'" +
+                    "AND fbpomd.ISPRIMARY = 'T'" +
+                    "AND fbpomd.MEMBERCLASS IS NULL";
+
+            String selectTableSQLForType_3 = "SELECT fbpomd.MANAGE_PERSON, fbpomd.MANAGE_STRUCTURE " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.MEMBERDATATYPE = '3'" +
+                    "AND fbpomd.ISPRIMARY = 'T'" +
+                    "AND fbpomd.MEMBERCLASS IS NULL";
+
+            Statement statement = connection.createStatement();
+
+            // Get data for type 1 from the database
+            ResultSet rs_1 = statement.executeQuery(selectTableSQLForType_1);
+
+            // if something was received then the while loop will work
+            while (rs_1.next()) {
+                managePerson_type_1 = rs_1.getString("MANAGE_PERSON");
+                manageStructure_type_1 = rs_1.getString("MANAGE_STRUCTURE");
+            }
+
+            // Get data for type 2 from the database
+            ResultSet rs_2 = statement.executeQuery(selectTableSQLForType_2);
+
+            // if something was received then the while loop will work
+            while (rs_2.next()) {
+                managePerson_type_2 = rs_2.getString("MANAGE_PERSON");
+                manageStructure_type_2 = rs_2.getString("MANAGE_STRUCTURE");
+            }
+
+            // Get data for type 3 from the database
+            ResultSet rs_3 = statement.executeQuery(selectTableSQLForType_3);
+
+            // if something was received then the while loop will work
+            while (rs_3.next()) {
+                managePerson_type_3 = rs_3.getString("MANAGE_PERSON");
+                manageStructure_type_3 = rs_3.getString("MANAGE_STRUCTURE");
+            }
+
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        if(Objects.equals(test_MANAGE_PERSON, DataComparison.compareData(managePerson_type_1, managePerson_type_2, managePerson_type_3))) {
+            System.out.println("MANAGE_PERSON test passed!");
+        } else {
+            System.out.println("MANAGE_PERSON test failed! - X");
+        }
+
+        if(Objects.equals(test_MANAGE_STRUCTURE, DataComparison.compareData(manageStructure_type_1, manageStructure_type_2, manageStructure_type_3))) {
+            System.out.println("MANAGE_STRUCTURE test passed!");
+        } else {
+            System.out.println("MANAGE_STRUCTURE test failed! - X");
+        }
 
     }
 }
