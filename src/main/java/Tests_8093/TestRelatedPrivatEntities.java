0f031174c7341580588653c6e475ec00e0fc0a1c
Eduard Anton <eduardanton78@gmail.com>
2021-03-11 12:55:14 +0700

    Clone TestRelatedPrivatEntities class (8092, 8093).

--- src/main/java/Tests_8093/TestRelatedPrivatEntities.java
+++ src/main/java/Tests_8093/TestRelatedPrivatEntities.java
@@ -1,4 +1,368 @@
 package Tests_8093;
 
+import Services.DataComparison;
+import Services.DateReplace;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class TestRelatedPrivatEntities {
+    final String DB_Data = "jdbc:oracle:thin:@server:1521:slx1";
+
+    String userName = "Admin";
+    String password = "4YFDtyiaPpvIbYkehzkG";
+    String requestMask = "UC-TSP";
+
+    String productOrderNumber = null;
+
+    String test_LASTNAME = null;
+    String test_FIRSTNAME = null;
+    String test_MIDDLENAME = null;
+    String test_BIRTHPLACE = null;
+    String test_BIRTHDATE = null;
+    String test_GENDER = null;
+    String test_INN = null;
+    String test_COUNTRY = null;
+
+    String lastName_type_1 = null;
+    String firstName_type_1 = null;
+    String middleName_type_1 = null;
+    String birthPlace_type_1 = null;
+    String birthDate_type_1 = null;
+    String gender_type_1 = null;
+    String inn_type_1 = null;
+    String country_type_1 = null;
+
+    String lastName_type_2 = null;
+    String firstName_type_2 = null;
+    String middleName_type_2 = null;
+    String birthPlace_type_2 = null;
+    String birthDate_type_2 = null;
+    String gender_type_2 = null;
+    String inn_type_2 = null;
+    String country_type_2 = null;
+
+    String lastName_type_3 = null;
+    String firstName_type_3 = null;
+    String middleName_type_3 = null;
+    String birthPlace_type_3 = null;
+    String birthDate_type_3 = null;
+    String gender_type_3 = null;
+    String inn_type_3 = null;
+    String country_type_3 = null;
+
+    @Test
+    public void testRelatedPrivatEntities() {
+
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\selenium\\drivers\\chromedriver_88\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(2000);
+            driver.get("http://192.168.1.140:8093/SlxClient/logoff.aspx");
+            driver.manage().window().maximize();
+
+            WebElement logoffHref = driver.findElement(By
+                    .linkText("Вернуться на страницу входа"));
+            logoffHref.click();
+
+            Thread.sleep(2000);
+
+            // Authorization in system
+            WebElement inputUserName = driver.findElement(By
+                    .xpath("//input[@name='ctl00$ContentPlaceHolderArea$slxLogin$UserName']"));
+            inputUserName.sendKeys(userName);
+
+            WebElement inputPassword = driver.findElement(By
+                    .xpath("//input[@name='ctl00$ContentPlaceHolderArea$slxLogin$Password']"));
+            inputPassword.sendKeys(password);
+
+            WebElement submitButton = driver.findElement(By
+                    .xpath("//input[@name='ctl00$ContentPlaceHolderArea$slxLogin$btnLogin']"));
+            submitButton.click();
+
+            Thread.sleep(2000);
+
+            // Enter to "Data change requests"
+            WebElement requestsHref = driver.findElement(By.xpath("//*[text()='Заявки']"));
+            requestsHref.click();
+
+            Thread.sleep(5000);
+
+            // Select UC requests
+            WebElement filterUC = driver.findElement(By.xpath("//*[@id='GroupLookupButton']"));
+            filterUC.click();
+
+            Thread.sleep(2000);
+
+            WebElement inputNumberOfRequest = driver.findElement(By
+                    .xpath("//*[@id='widget_GroupLookup-ConditionManager-SearchCondition0-TextValue']/div/input"));
+            inputNumberOfRequest.sendKeys(requestMask);
+
+            WebElement submitSelect = driver.findElement(By
+                    .xpath("//*[@id='GroupLookup-ConditionManager-Search_label']"));
+            submitSelect.click();
+            Thread.sleep(2000);
+
+            // Sort search result
+            WebElement sortByModifiedDate = driver.findElement(By.xpath("//*[@id='dojoUnique27']/div"));
+            sortByModifiedDate.click();
+            Thread.sleep(1000);
+            sortByModifiedDate.click();
+            Thread.sleep(5000);
+
+            // Open the last application
+            //WebElement lastApplication = driver.findElement(By.xpath("//*[@id='listGrid-row-:r6AiXwz1uQM=']/table/tr/td[1]/a"));
+            WebElement lastApplication = driver.findElement(By
+                    .xpath("//div[@id='dijit_layout_StackContainer_0']//td/a"));
+            lastApplication.click();
+            Thread.sleep(2000);
+
+            // Get data of the PRODUCTORDERNUMBER
+            WebElement field_PRODUCTORDERNUMBER = driver.findElement(By
+                    .xpath("//*[@id='PageTitle']"));
+            String productOrderNumberFull = field_PRODUCTORDERNUMBER.getText();
+            // Отрезать "Заявка - "
+            String [] splitString = productOrderNumberFull.split(" ");
+            productOrderNumber = splitString[2];
+            Thread.sleep(3000);
+
+            // Select the Related Privat Entities tab
+            WebElement relatedPrivatEntitiesTab = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[1]/div/div[4]/a"));
+            relatedPrivatEntitiesTab.click();
+            Thread.sleep(2000);
+
+            // Disclose data on the Related Privat Entities - " V "
+            WebElement openrelatedPrivatEntities = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div/div/i"));
+            openrelatedPrivatEntities.click();
+            Thread.sleep(3000);
+
+            // Get data of the Related Privat Entities type
+            WebElement field_LASTNAME = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/textarea"));
+            test_LASTNAME = field_LASTNAME.getAttribute("value");
+
+            WebElement field_FIRSTNAME = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[2]/div[1]/div/div/textarea"));
+            test_FIRSTNAME = field_FIRSTNAME.getAttribute("value");
+
+            WebElement field_MIDDLENAME = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[3]/div[1]/div/div/textarea"));
+            test_MIDDLENAME = field_MIDDLENAME.getAttribute("value");
+
+            WebElement field_BIRTHPLACE = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[4]/div[1]/div/div/div/div/div/div/input"));
+            test_BIRTHPLACE = field_BIRTHPLACE.getAttribute("value");
+
+            WebElement field_BIRTHDATE = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[5]/div[1]/div/div/div/div/div/div[1]/input"));
+            test_BIRTHDATE = field_BIRTHDATE.getAttribute("value");
+
+            WebElement field_GENDER = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[6]/div[1]/div/div/div/div/div/div[1]/input"));
+            test_GENDER = field_GENDER.getAttribute("value");
+
+            WebElement field_INN = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[7]/div[1]/div/div/textarea"));
+            test_INN = field_INN.getAttribute("value");
+
+            WebElement field_COUNTRY = driver.findElement(By
+                    .xpath("//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div/div/div[2]/div[2]/div[8]/div[1]/div/div/div/div/div/div[1]/input"));
+            test_COUNTRY = field_COUNTRY.getAttribute("value");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            driver.close();
+        }
+
+        System.out.println(productOrderNumber);
+        System.out.println("---------------");
+        System.out.println(test_LASTNAME);
+        System.out.println(test_FIRSTNAME);
+        System.out.println(test_MIDDLENAME);
+        System.out.println(test_BIRTHPLACE);
+        System.out.println(test_BIRTHDATE);
+        System.out.println(test_GENDER);
+        System.out.println(test_INN);
+        System.out.println(test_COUNTRY);
+        System.out.println("---------------");
+
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection(DB_Data, "SYSDBA", "masterkey");
+
+            String selectTableSQLForType_1 = "SELECT fbpomd.LASTNAME, " +
+                    "fbpomd.FIRSTNAME, " +
+                    "fbpomd.MIDDLENAME, " +
+                    "fbpomd.BIRTHPLACE, " +
+                    "fbpomd.BIRTHDATE, " +
+                    "fbpomd.GENDER, " +
+                    "fbpomd.INN, " +
+                    "fbpomd.COUNTRY " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.isprimary = 'F' " +
+                    "AND fbpomd.memberclass = 'CL_PRIV' " +
+                    "AND fbpomd.INN = '" + test_INN + "' " +
+                    "AND fbpomd.memberdatatype = '1'";
+
+            String selectTableSQLForType_2 = "SELECT fbpomd.LASTNAME, " +
+                    "fbpomd.FIRSTNAME, " +
+                    "fbpomd.MIDDLENAME, " +
+                    "fbpomd.BIRTHPLACE, " +
+                    "fbpomd.BIRTHDATE, " +
+                    "fbpomd.GENDER, " +
+                    "fbpomd.INN, " +
+                    "fbpomd.COUNTRY " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.isprimary = 'F' " +
+                    "AND fbpomd.memberclass = 'CL_PRIV' " +
+                    "AND fbpomd.INN = '" + test_INN + "' " +
+                    "AND fbpomd.memberdatatype = '2'";
+
+            String selectTableSQLForType_3 = "SELECT fbpomd.LASTNAME, " +
+                    "fbpomd.FIRSTNAME, " +
+                    "fbpomd.MIDDLENAME, " +
+                    "fbpomd.BIRTHPLACE, " +
+                    "fbpomd.BIRTHDATE, " +
+                    "fbpomd.GENDER, " +
+                    "fbpomd.INN, " +
+                    "fbpomd.COUNTRY " +
+                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
+                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
+                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
+                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
+                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
+                    "WHERE fbpo.PRODUCTORDERNUMBER = '" + productOrderNumber + "'" +
+                    "AND fbpomd.isprimary = 'F' " +
+                    "AND fbpomd.memberclass = 'CL_PRIV' " +
+                    "AND fbpomd.INN = '" + test_INN + "' " +
+                    "AND fbpomd.memberdatatype = '3'";
+
+            Statement statement = connection.createStatement();
+
+            // Get data for type 1 from the database
+            ResultSet rs_1 = statement.executeQuery(selectTableSQLForType_1);
+
+            // if something was received then the while loop will work
+            while (rs_1.next()) {
+                lastName_type_1 = rs_1.getString("LASTNAME");
+                firstName_type_1 = rs_1.getString("FIRSTNAME");
+                middleName_type_1 = rs_1.getString("MIDDLENAME");
+                birthPlace_type_1 = rs_1.getString("BIRTHPLACE");
+                birthDate_type_1 = DateReplace.replaceInputDate(rs_1.getString("BIRTHDATE"));
+                gender_type_1 = rs_1.getString("GENDER");
+                inn_type_1 = rs_1.getString("INN");
+                country_type_1 = rs_1.getString("COUNTRY");
+            }
+
+            // Get data for type 2 from the database
+            ResultSet rs_2 = statement.executeQuery(selectTableSQLForType_2);
+
+            // if something was received then the while loop will work
+            while (rs_2.next()) {
+                lastName_type_2 = rs_2.getString("LASTNAME");
+                firstName_type_2 = rs_2.getString("FIRSTNAME");
+                middleName_type_2 = rs_2.getString("MIDDLENAME");
+                birthPlace_type_2 = rs_2.getString("BIRTHPLACE");
+                birthDate_type_2 = DateReplace.replaceInputDate(rs_2.getString("BIRTHDATE"));
+                gender_type_2 = rs_2.getString("GENDER");
+                inn_type_2 = rs_2.getString("INN");
+                country_type_2 = rs_2.getString("COUNTRY");
+            }
+
+            // Get data for type 3 from the database
+            ResultSet rs_3 = statement.executeQuery(selectTableSQLForType_3);
+
+            // if something was received then the while loop will work
+            while (rs_3.next()) {
+                lastName_type_3 = rs_3.getString("LASTNAME");
+                firstName_type_3 = rs_3.getString("FIRSTNAME");
+                middleName_type_3 = rs_3.getString("MIDDLENAME");
+                birthPlace_type_3 = rs_3.getString("BIRTHPLACE");
+                birthDate_type_3 = DateReplace.replaceInputDate(rs_3.getString("BIRTHDATE"));
+                gender_type_3 = rs_3.getString("GENDER");
+                inn_type_3 = rs_3.getString("INN");
+                country_type_3 = rs_3.getString("COUNTRY");
+            }
+
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        if(Objects.equals(test_LASTNAME, DataComparison.compareData(lastName_type_1, lastName_type_2, lastName_type_3))) {
+            System.out.println("LASTNAME test passed!");
+        }else {
+            System.out.println("LASTNAME test failed! - X");
+        }
+
+        if(Objects.equals(test_FIRSTNAME, DataComparison.compareData(firstName_type_1, firstName_type_2, firstName_type_3))) {
+            System.out.println("FIRSTNAME test passed!");
+        }else {
+            System.out.println("FIRSTNAME test failed! - X");
+        }
+
+        if(Objects.equals(test_MIDDLENAME, DataComparison.compareData(middleName_type_1, middleName_type_2, middleName_type_3))) {
+            System.out.println("MIDDLENAME test passed!");
+        }else {
+            System.out.println("MIDDLENAME test failed! - X");
+        }
+
+        if(Objects.equals(test_BIRTHPLACE, DataComparison.compareData(birthPlace_type_1, birthPlace_type_2, birthPlace_type_3))) {
+            System.out.println("BIRTHPLACE test passed!");
+        }else {
+            System.out.println("BIRTHPLACE test failed! - X");
+        }
+
+        if(Objects.equals(test_BIRTHDATE, DataComparison.compareData(birthDate_type_1, birthDate_type_2, birthDate_type_3))) {
+            System.out.println("BIRTHDATE test passed!");
+        }else {
+            System.out.println("BIRTHDATE test failed! - X");
+        }
+
+        if(Objects.equals(test_GENDER, DataComparison.compareData(gender_type_1, gender_type_2, gender_type_3))) {
+            System.out.println("GENDER test passed!");
+        }else {
+            System.out.println("GENDER test failed! - X");
+        }
+
+        if(Objects.equals(test_INN, DataComparison.compareData(inn_type_1, inn_type_2, inn_type_3))) {
+            System.out.println("INN test passed!");
+        }else {
+            System.out.println("INN test failed! - X");
+        }
+
+        if(Objects.equals(test_COUNTRY, DataComparison.compareData(country_type_1, country_type_2, country_type_3))) {
+            System.out.println("COUNTRY test passed!");
+        }else {
+            System.out.println("COUNTRY test failed! - X");
+        }
+
+        System.out.println("----------------------------");
+    }
 }
