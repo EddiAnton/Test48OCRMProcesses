1bf9fbef24ef4f3e426d94bf1514a4c8f0ef8bdd
Eduard Anton <eduardanton78@gmail.com>
2021-03-17 15:17:34 +0700

    Coding TestOKVED, XpathOKVED classes to 8083.

---
+++ src/main/java/Services/XpathOKVED.java
@@ -0,0 +1,21 @@
+package Services;
+
+public class XpathOKVED {
+
+    // XPATH the OKVED fields
+
+    public static final String OKVED_TAB = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[1]/div/div[10]/a";
+
+    public static final String OPEN_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[1]/div";
+
+    public static final String FIELD_CODE_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[2]/div[2]/div[1]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CODENAME_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[2]/div[2]/div[2]/div[1]/div/div/textarea";
+
+    public static final String FIELD_BEGINDATE_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_ENDDATE_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_ISPRIMARY_OKVED = "//*[@id=\"DetailProductOrderChangeComponent\"]/div[2]/section/div/div[7]/div[1]/div[2]/div[2]/div[4]/div[1]/div/div/div/div/input";
+
+}
