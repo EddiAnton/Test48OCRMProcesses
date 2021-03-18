a45304ec61686eee6c1a6eacf3dd3747a6e01c3c
Eduard Anton <eduardanton78@gmail.com>
2021-03-19 01:02:30 +0700

    Coding TestUpdatingOtherInformation and XpathUpdatingOtherInformation classes to 8083 package.

---
+++ src/main/java/Services/XpathUpdatingOtherInformation.java
@@ -0,0 +1,45 @@
+package Services;
+
+public class XpathUpdatingOtherInformation {
+
+    public static final String UPDATING_OTHER_INFORMATION_TAB  = "//*[@id='DetailProductOrderChangeComponent']/div[1]/div/div[11]/a";
+
+    public static final String FIELD_ISREGOFFSHORZONE = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[1]/div[1]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_OKPO = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[1]/div[2]/div[1]/div/div/textarea";
+
+    public static final String FIELD_OKOGU = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[1]/div[3]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_SWIFT = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[1]/div[4]/div[1]/div/div/textarea";
+
+    public static final String FIELD_EIN = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[1]/div[1]/div/div/textarea";
+
+    public static final String FIELD_GIIN = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[2]/div[1]/div/div/textarea";
+
+    public static final String FIELD_FATCADOCW9 = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[3]/div[1]/div/div/div/div/input";
+
+    public static final String FIELD_FATCADOC = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[4]/div[1]/div/div/div/div/input";
+
+    public static final String FIELD_DISREGARDED = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[5]/div[1]/div/div/div/div/input";
+
+    public static final String FIELD_FATCASTATUS = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[2]/div[6]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String OPEN_CRS = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div/div";
+
+    public static final String FIELD_CRS_NALOG_RESIDENCE = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CRS_IINN_TYPE = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CRS_IINN = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[3]/div[1]/div/div/textarea";
+
+    public static final String FIELD_CRS_NO_IINN_CODE = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[4]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CRS_NO_IINN_REASON = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[5]/div[1]/div/div/textarea";
+
+    public static final String FIELD_CRS_DATE_BEGIN = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[6]/div[1]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CRS_DATE_END = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[6]/div[2]/div[1]/div/div/div/div/div/div[1]/input";
+
+    public static final String FIELD_CRS_STATUS = "//*[@id='DetailProductOrderChangeComponent']/div[2]/section/div/div[9]/div[4]/div/div[2]/div[2]/div[7]/div[1]/div/div/div/div/div/div[1]/input";
+
+}
