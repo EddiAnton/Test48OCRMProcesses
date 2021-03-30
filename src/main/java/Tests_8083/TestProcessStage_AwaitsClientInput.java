55978f9783a08ff1cf24e3f70c895be2754d4918
Eduard Anton <eduardanton78@gmail.com>
2021-03-30 21:25:11 +0700

    Coding TestProcessStage class 8083.

--- src/main/java/Tests_8083/TestProcessStage_AwaitsClientInput.java
+++ src/main/java/Tests_8083/TestProcessStage_AwaitsClientInput.java
@@ -3,15 +3,17 @@ package Tests_8083;
 import Services.DataConversion;
 import Services.XpathAuthorization;
 import Services.XpathTestProcessStage;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
-import org.openqa.selenium.support.ui.Select;
-
 import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestProcessStage_AwaitsClientInput {
 
@@ -21,8 +23,11 @@ public class TestProcessStage_AwaitsClientInput {
     String requestMask = "UC-TSP";
 
     String productOrderNumber = null;
+    String fb_productOrderID = null;
 
+    String applicationStage = null;
     String applicationStatus = null;
+    String newApplicationStage = null;
     String newApplicationStatus = null;
 
     @Test
@@ -92,15 +97,41 @@ public class TestProcessStage_AwaitsClientInput {
             WebElement field_PRODUCTORDERNUMBER = driver.findElement(By
                     .xpath(XpathAuthorization.FIELD_PRODUCTORDERNUMBER));
             productOrderNumber = DataConversion.getProductOrderNumber(field_PRODUCTORDERNUMBER.getText());
-            System.out.println(productOrderNumber);
+            // Get data of the FB_PRODUCTORDERID
+            WebElement field_PRODUCTORDER = driver.findElement(By
+                    .xpath(XpathAuthorization.FIELD_PRODUCTORDER));
+            fb_productOrderID = DataConversion.getFB_ProductOrderID(field_PRODUCTORDER.getAttribute("action"));
             Thread.sleep(3000);
+
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection(DB_Data, "SYSDBA", "masterkey");
+
+            Statement statement = connection.createStatement();
+
+            String selectTableSQLForStageName = "SELECT ps1.name AS StageName " +
+                    "FROM sysdba.fb_processstageaudit psa1 " +
+                    "INNER JOIN sysdba.fb_processaudit pa1 " +
+                    "ON (psa1.fb_processauditid = pa1.fb_processauditid) " +
+                    "INNER JOIN sysdba.fb_processstage ps1 " +
+                    "ON (psa1.currentstageid = ps1.fb_processstageid) " +
+                    "WHERE pa1.entityid = '" + fb_productOrderID + "' " +
+                    "ORDER BY psa1.createdate " +
+                    "DESC " +
+                    "FETCH NEXT 1 ROWS ONLY";
+
+            ResultSet rs_stageName = statement.executeQuery(selectTableSQLForStageName);
+            while (rs_stageName.next()) {
+                applicationStage = rs_stageName.getString("STAGENAME");
+            }
+
 
             // Get data of the application Status
             WebElement field_applicationStatus = driver.findElement(By
                     .xpath(XpathTestProcessStage.FIELD_APPLICATION_STATUS));
             applicationStatus = field_applicationStatus.getText();
 
-            if (applicationStatus.equals("В РАБОТЕ") || applicationStatus.equals("ДОРАБОТКА")) {
+            if (applicationStatus.equals("В работе") || applicationStatus.equals("ДОРАБОТКА")) {
 
                 // Get data of the application Status
                 WebElement continueRegistration  = driver.findElement(By.xpath(XpathTestProcessStage.BUTTON_CONTINUE_REGISTRATION));
@@ -108,9 +139,6 @@ public class TestProcessStage_AwaitsClientInput {
                 Thread.sleep(2000);
 
                 // Selecting a stage from the drop-down list
-                // Select statusList = new Select(driver.findElement(By.xpath(XpathTestProcessStage.OPEN_STATUS_LIST)));
-                // statusList.selectByVisibleText("Ожидание информации от клиента");
-
                 driver.findElement(By.xpath(XpathTestProcessStage.OPEN_STATUS_LIST)).click();
                 Thread.sleep(1000);
                 driver.findElement(By.xpath(XpathTestProcessStage.STATUS_AWAITS_CLIENT_INPUT)).click();
@@ -127,14 +155,24 @@ public class TestProcessStage_AwaitsClientInput {
 
             }
 
-        } catch (InterruptedException e) {
+            ResultSet rs_newStageName = statement.executeQuery(selectTableSQLForStageName);
+            while (rs_newStageName.next()) {
+                newApplicationStage = rs_newStageName.getString("STAGENAME");
+            }
+            connection.close();
+
+        } catch (InterruptedException | SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             driver.close();
         }
 
         System.out.println("---------------");
-        System.out.println("Текущий статус: " + applicationStatus);
+        System.out.println(productOrderNumber);
+        System.out.println("---------------");
+        System.out.println("Начальная стадия: " + applicationStage);
+        System.out.println("Начальный статус: " + applicationStatus);
+        System.out.println("Новая стадия: " + newApplicationStage);
         System.out.println("Новый статус: " + newApplicationStatus);
         System.out.println("---------------");
 
