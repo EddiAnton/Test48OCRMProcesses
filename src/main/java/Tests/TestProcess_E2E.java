2d49b90434004697f7f378105504b71883d88559
Eduard Anton <eduardanton78@gmail.com>
2021-04-21 19:20:52 +0700

    The function of modeling the response from CFT has been moved to a separate class of the Service package.

--- src/main/java/Tests/TestProcess_E2E.java
+++ src/main/java/Tests/TestProcess_E2E.java
@@ -1,37 +1,18 @@
 package Tests;
 
-import Services.DataConversion;
-import Services.Rerty;
-import Services.TestEnvironment;
-import Services.XpathAuthorization;
-import Services.XpathCreateApplicate;
-import Services.XpathTestProcessStage;
-
+import Services.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 
 public class TestProcess_E2E {
 
@@ -354,76 +335,7 @@ public class TestProcess_E2E {
 
             /** Modeling a response from CFT about a successful update. **/
 
-
-            // Получаем ID всех записей с 3 типом в заявке
-            String selectForAll_FB_PRODUCTORDMEMB_DATAID = "SELECT fbpomd.FB_PRODUCTORDMEMB_DATAID " +
-                    "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
-                    "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
-                    "ON fbpomd.FB_PRODUCTORDERMEMBERID = fbpom.FB_PRODUCTORDERMEMBERID " +
-                    "JOIN SYSDBA.FB_PRODUCTORDER fbpo " +
-                    "ON fbpom.FB_PRODUCTORDERID = fbpo.FB_PRODUCTORDERID " +
-                    "WHERE fbpo.FB_PRODUCTORDERID = '" + fb_productOrderID + "' " +
-                    "AND fbpomd.MEMBERDATATYPE = 3";
-
-            List <String> DATAID_list = new ArrayList<>();
-            ResultSet rs_allData = statement_SYSDBA.executeQuery(selectForAll_FB_PRODUCTORDMEMB_DATAID);
-            while (rs_allData.next()) {
-                DATAID_list.add(rs_allData.getString("FB_PRODUCTORDMEMB_DATAID"));
-            }
-
-
-            // Read the script into a variable
-            File cft_response_file = new File("Response_From_CFT_Receiving.sql");
-            BufferedReader reader_cft_response = new BufferedReader(new InputStreamReader(new FileInputStream(cft_response_file), "windows-1251"));
-            String line_cft_response;
-            StringBuilder sb_cft_response = new StringBuilder();
-            while ((line_cft_response = reader_cft_response.readLine()) != null) {
-                sb_cft_response.append(line_cft_response);
-                sb_cft_response.append(ls);
-            }
-            String SQL_CFT_Response = sb_cft_response.toString();
-            reader_cft_response.close();
-
-
-            // Read the xml-file into a variable
-            File cft_file = new File("CFT_response.xml");
-            BufferedReader cft_reader = new BufferedReader(new InputStreamReader(new FileInputStream(cft_file), "UTF-8"));
-            String line_cft;
-            StringBuilder sb_cft = new StringBuilder();
-            while ((line_cft = cft_reader.readLine()) != null) {
-                sb_cft.append(line_cft);
-                sb_cft.append(ls);
-            }
-            cft_reader.close();
-            Thread.sleep(23000);
-
-            // For each entry in the application
-            for (String data: DATAID_list) {
-                // Insert fb_productOrderID to script
-                String CFT_response = sb_cft.toString();
-                CFT_response = CFT_response.replace("Infor_ID", data);
-
-                //Update IN_MSG field into NM_CRM.EVENT_TABLE
-                PreparedStatement ps_CFT = connection_NM_CRM.prepareStatement(
-                        "UPDATE NM_CRM.EVENT_TABLE " +
-                                "SET " +
-                                "IN_MSG = '" + CFT_response + "' " +
-                                "WHERE ID = '5'"
-                );
-                ps_CFT.execute();
-
-
-                // Insert response to DB
-                PreparedStatement ps_CFT_response = connection_NM_CRM.prepareStatement(SQL_CFT_Response);
-                ps_CFT_response.execute();
-                Thread.sleep(3000);
-
-                System.out.println();
-                System.out.println("-> Получен ответ от ЦФТ о успешном обновлении данных " + data + " ->");
-                System.out.println();
-
-            }
-
+            ModelingCFT.responseCFT(fb_productOrderID, connection_NM_CRM, connection_SYSDBA);
             Thread.sleep(10000);
             driver.navigate().refresh();
 
