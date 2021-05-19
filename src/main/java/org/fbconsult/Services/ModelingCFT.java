367cb4b4c77070db0a8a275fd754134343eed97f
Eduard Anton <eduardanton78@gmail.com>
2021-05-19 13:20:23 +0700

    Update TestProcess_BP.

--- src/main/java/org/fbconsult/Services/ModelingCFT.java
+++ src/main/java/org/fbconsult/Services/ModelingCFT.java
@@ -109,6 +109,7 @@ public final class ModelingCFT {
 
             Statement statement_SYSDBA = connection_SYSDBA.createStatement();
 
+            // КЛАДЕМ В ОЧЕРЕДЬ ОСНОВНОЕ ЛИЦО
             // Read the CFT_MainClient_part1.xml into a variable
             File mainClient_file_1 = new File("CFT_MainClient_part1.xml");
             BufferedReader mainClient_reader_1 = new BufferedReader(new InputStreamReader(new FileInputStream(mainClient_file_1), StandardCharsets.UTF_8));
@@ -169,14 +170,15 @@ public final class ModelingCFT {
             // Insert response to DB
             PreparedStatement ps_mainClient_Response = connection_NM_CRM.prepareStatement(SQL_MainClient_Response);
             ps_mainClient_Response.execute();
-            Thread.sleep(20000);
+            Thread.sleep(10000);
 
             System.out.println();
             System.out.println("-> Получены данные из ЦФТ по основному клиенту ->");
             System.out.println();
 
 
-            // Получаем ID всех записей по связанным лицам в заявке
+            // КЛАДЕМ В ОЧЕРЕДЬ СВЯЗАННОЕ ЮРИДИЧЕСКОЕ ЛИЦО
+            // Получаем ID связанного юридического лица в заявке
             String selectForAll_FB_PRODUCTORDMEMB_DATAID = "SELECT fbpomd.FB_PRODUCTORDMEMB_DATAID " +
                     "FROM SYSDBA.FB_PRODUCTORDMEMB_DATA fbpomd " +
                     "JOIN SYSDBA.FB_PRODUCTORDERMEMBER fbpom " +
@@ -186,11 +188,79 @@ public final class ModelingCFT {
                     "WHERE fbpo.FB_PRODUCTORDERID = '" + fb_productOrderID + "' " +
                     "AND fbpomd.IDCFT = '" + CFTID_RelatedLegalEntity + "'";
 
-            String DATAID = null;
+            String DATA_RelatedLegalEntity_ID = null;
             ResultSet rs_DataID = statement_SYSDBA.executeQuery(selectForAll_FB_PRODUCTORDMEMB_DATAID);
             while (rs_DataID.next()) {
-                DATAID = rs_DataID.getString("FB_PRODUCTORDMEMB_DATAID");
+                DATA_RelatedLegalEntity_ID = rs_DataID.getString("FB_PRODUCTORDMEMB_DATAID");
             }
+
+
+            // Read the CFT_RelatedLegalEntity_part1.xml into a variable
+            File relatedLegalEntity_file_1 = new File("CFT_RelatedLegalEntity_part1.xml");
+            BufferedReader relatedLegalEntity_reader_1 = new BufferedReader(new InputStreamReader(new FileInputStream(relatedLegalEntity_file_1), StandardCharsets.UTF_8));
+            String line_relatedLegalEntity_file_1;
+            StringBuilder sb_relatedLegalEntity_1 = new StringBuilder();
+            while ((line_relatedLegalEntity_file_1 = relatedLegalEntity_reader_1.readLine()) != null) {
+                sb_relatedLegalEntity_1.append(line_relatedLegalEntity_file_1);
+                sb_relatedLegalEntity_1.append(separator);
+            }
+            // Insert fb_productOrderIDr to xml
+            String RelatedLegalEntity_response_1 = sb_relatedLegalEntity_1.toString();
+            assert DATA_RelatedLegalEntity_ID != null;
+            RelatedLegalEntity_response_1 = RelatedLegalEntity_response_1.replace("Infor_ID", DATA_RelatedLegalEntity_ID);
+            relatedLegalEntity_reader_1.close();
+
+
+            // Read the CFT_RelatedLegalEntity_part2.xml into a variable
+            File relatedLegalEntity_file_2 = new File("CFT_RelatedLegalEntity_part2.xml");
+            BufferedReader relatedLegalEntity_reader_2 = new BufferedReader(new InputStreamReader(new FileInputStream(relatedLegalEntity_file_2), StandardCharsets.UTF_8));
+            String line_relatedLegalEntity_file_2;
+            StringBuilder sb_relatedLegalEntity_2 = new StringBuilder();
+            while ((line_relatedLegalEntity_file_2 = relatedLegalEntity_reader_2.readLine()) != null) {
+                sb_relatedLegalEntity_2.append(line_relatedLegalEntity_file_2);
+                sb_relatedLegalEntity_2.append(separator);
+            }
+            String RelatedLegalEntity_response_2 = sb_relatedLegalEntity_2.toString();
+            relatedLegalEntity_reader_2.close();
+
+
+            //Update IN_MSG field into NM_CRM.EVENT_TABLE
+            // Read the sql-file into a variable
+            File script_file_rle = new File("Script_to_CLOB_2.sql");
+            BufferedReader script_reader_rle = new BufferedReader(new InputStreamReader(new FileInputStream(script_file_rle), "windows-1251"));
+            String line_script_rle;
+            StringBuilder sb_script_rle = new StringBuilder();
+            while ((line_script_rle = script_reader_rle.readLine()) != null) {
+                sb_script_rle.append(line_script_rle);
+                sb_script_rle.append(separator);
+            }
+
+            String Script_to_CLOB_2_rle = sb_script_rle.toString().replace("Very_long_string_value_1", RelatedLegalEntity_response_1);
+            Script_to_CLOB_2_rle = Script_to_CLOB_2_rle.replace("Very_long_string_value_2", RelatedLegalEntity_response_2);
+            PreparedStatement ps_script_rle = connection_NM_CRM.prepareStatement(Script_to_CLOB_2_rle);
+            ps_script_rle.execute();
+            script_reader_rle.close();
+
+
+            // Read the script into a variable
+            File relatedLegalEntity_response_file = new File("Response_From_CFT_REQ_Receiving.sql");
+            BufferedReader reader_relatedLegalEntity_response = new BufferedReader(new InputStreamReader(new FileInputStream(relatedLegalEntity_response_file), "windows-1251"));
+            String line_relatedLegalEntity_response;
+            StringBuilder stringBuilder_relatedLegalEntity = new StringBuilder();
+            while ((line_relatedLegalEntity_response = reader_relatedLegalEntity_response.readLine()) != null) {
+                stringBuilder_relatedLegalEntity.append(line_relatedLegalEntity_response);
+                stringBuilder_relatedLegalEntity.append(separator);
+            }
+            String SQL_RelatedLegalEntity_Response = stringBuilder_relatedLegalEntity.toString();
+            reader_relatedLegalEntity_response.close();
+            // Insert response to DB
+            PreparedStatement ps_relatedLegalEntity_Response = connection_NM_CRM.prepareStatement(SQL_RelatedLegalEntity_Response);
+            ps_relatedLegalEntity_Response.execute();
+            Thread.sleep(10000);
+
+            System.out.println();
+            System.out.println("-> Получены данные из ЦФТ по связанному юридическому лицу ->");
+            System.out.println();
 
         } catch (InterruptedException | SQLException | IOException e) {
             e.printStackTrace();
